f21cc156dec972f26ee72ad26a38e3d6fa7de1ea
taony <taonyzhang@gmail.com>
2017-07-10 23:40:39 +0800

    context

--- TaonyNet.Core/Blog/Blog.cs
+++ TaonyNet.Core/Blog/Blog.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Abp.Domain.Entities;
 using Abp.Domain.Entities.Auditing;
 
